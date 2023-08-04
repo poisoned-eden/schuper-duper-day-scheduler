a8329a9af4903bec8812d0a45189f2a7466e0b96
poisoned-eden <poisoned.eden@gmail.com>
2023-08-04 16:59:00 +0100

    gets from local storage and removes comments and console.logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,14 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
 
-
-	// TODO: Add code to get any user input that was saved in localStorage and set
-	// the values of the corresponding textarea elements. HINT: How can the id
-	// attribute of each time-block be used to do this?
-	//
-	// TODO: Add code to display the current date in the header of the page.
-
-	let date = dayjs().format('MMM D, YYYY');
-	let time = dayjs().format('hh:mm');
+	let time = dayjs().format('HH:mm');
 	let hour = dayjs().format('HH');
-	let hourAMPM = dayjs().format('HH A');
-	$('#currentDay').text(date);
+
 	$('#currentTime').text(time);
 
 
 	for (let i = 9; i < 18; i++) {
 		let tense;
+		let hourLabel = `hour-${i}`;
 		
 		if (i < hour) {
 			tense = 'past';
@@ -29,39 +18,34 @@ $(function () {
 			tense = 'future';
 		}
 
-		let newTimeBlock = $(`<div id="hour-${i}" class="row time-block ${tense}">
-        <div class="col-2 col-md-1 hour text-center py-3">${i}</div>
-        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-          <i class="fas fa-save" aria-hidden="true"></i>
-        </button>
-      </div>`)
+		let savedBlockData = localStorage.getItem(hourLabel);
+		if ( savedBlockData == null ) {
+			savedBlockData = ''
+		}
+
+		let newTimeBlock = $(`
+			<div id="${hourLabel}" class="row time-block ${tense}">
+        		<div class="col-2 col-md-1 hour text-center py-3">${i}</div>
+					<textarea class="col-8 col-md-10 description" rows="3">${savedBlockData}</textarea>
+					<button class="btn saveBtn col-2 col-md-1" aria-label="save">
+						<i class="fas fa-save" aria-hidden="true"></i>
+					</button>
+      			</div>`);
 	  
 	  $('#schedule').append(newTimeBlock);
 	}
   
 	function saveHandler(event) {
-	console.log(this);
-	
-		console.log(event.target);
 		let clickedEl = $(event.target);
 
 		let parentDivEl = clickedEl.parents("div:first");
-		console.log(parentDivEl);
-
-		let timeBlock = parentDivEl.attr('id');
-		console.log(timeBlock);
-
-		let blockInput = parentDivEl.children('textarea').val();
-		console.log('input '+ blockInput);
-
-		localStorage.setItem(timeBlock, blockInput);
-		console.log('ls ' + localStorage.getItem(timeBlock));
 	
+		let timeBlock = parentDivEl.attr('id');
+	
+		let blockInput = parentDivEl.children('textarea').val();
+	
+		localStorage.setItem(timeBlock, blockInput);
 	}
 	
   $('#schedule').on('click', saveHandler);
 });
-
-
-// ? set a timeout/interval to change the colours every hour
