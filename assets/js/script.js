41c6233075a9d8a8ea57bd5bd2171c74c3f2f051
poisoned-eden <poisoned.eden@gmail.com>
2023-08-04 10:53:13 +0100

    adds for loop to create time blocks with relevant classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,8 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-	// TODO: Add code to apply the past, present, or future class to each time
-	// block by comparing the id to the current hour. HINTS: How can the id
-	// attribute of each time-block be used to conditionally add or remove the
-	// past, present, and future classes? How can Day.js be used to get the
-	// current hour in 24-hour time?
-	//
+
+
 	// TODO: Add code to get any user input that was saved in localStorage and set
 	// the values of the corresponding textarea elements. HINT: How can the id
 	// attribute of each time-block be used to do this?
@@ -16,8 +12,33 @@ $(function () {
 
 	let date = dayjs().format('MMM D, YYYY');
 	let time = dayjs().format('hh:mm');
+	let hour = dayjs().format('HH');
+	let hourAMPM = dayjs().format('HH A');
 	$('#currentDay').text(date);
 	$('#currentTime').text(time);
+
+
+	for (let i = 9; i < 18; i++) {
+		let tense;
+		
+		if (i < hour) {
+			tense = 'past';
+		} else if ( i == hour ) {
+			tense = 'present';
+		} else {
+			tense = 'future';
+		}
+
+		let newTimeBlock = $(`<div id="hour-${i}" class="row time-block ${tense}">
+        <div class="col-2 col-md-1 hour text-center py-3">${i}</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+          <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+      </div>`)
+	  
+	  $('#schedule').append(newTimeBlock);
+	}
   
 	function saveHandler(event) {
 	console.log(this);
